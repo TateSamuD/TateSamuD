029029208bbbc493bb9b29250eb4311c9da8fb0d
Tatenda Samudzi <tatenda.samudzi@gmail.com>
2023-12-09 23:08:23 -0600

    readme update

---
+++ update-readme.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const { execSync } = require("child_process");
+
+// Fetch language statistics using GitHub API
+const getLanguageStats = () => {
+  const result = execSync("npx github-lang-stats TateSamuD");
+  return JSON.parse(result.toString());
+};
+
+// Generate README content
+const generateReadmeContent = () => {
+  const languageStats = getLanguageStats();
+
+  let content = `
+    # Hello, I'm Tatenda Samudzi
+
+    ## My GitHub Stats
+
+    `;
+
+  // Append language statistics
+  for (const lang in languageStats) {
+    content += `- ${lang}: ${languageStats[lang]}\n`;
+  }
+
+  fs.writeFileSync("README.md", content.trim());
+};
+
+generateReadmeContent();
